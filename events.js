2d146259ecff88270c2518ac3ff7b90bde2b61e3
Aidan Rubenstein <ar2126@rit.edu>
2019-02-25 14:31:08 -0500

    Update events.js

--- events.js
+++ events.js
@@ -7,6 +7,10 @@ AFRAME.registerComponent('markerhandler', {
         const animatedMarker = document.querySelector("#links");
 
         const aEntity = document.querySelector("#github");
+        
+        const aEntity2 = document.querySelector("#twitter");
+
+        const aEntity3 = document.querySelector("#linkedin");
 
 
 
@@ -18,6 +22,20 @@ AFRAME.registerComponent('markerhandler', {
 
             if (aEntity && intersectedElement === aEntity) {
                 window.open("https://www.github.com/ar2126");
+            }
+            else if (aEntity2 && intersectedElement === aEntity2) {
+                const scale = aEntity2.getAttribute('scale');
+
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+
+                aEntity2.setAttribute('scale', scale);
+            }
+            else if (aEntity3 && intersectedElement === aEntity3) {
+                const scale = aEntity3.getAttribute('scale');
+
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+
+                aEntity3.setAttribute('scale', scale);
             }
 
         });

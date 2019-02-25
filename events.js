16d45371342236c18bfd0ea5456f3df958077ca1
Aidan Rubenstein <ar2126@rit.edu>
2019-02-25 12:48:50 -0500

    Update events.js

--- events.js
+++ events.js
@@ -1,23 +1,31 @@
-//register the active marker
 AFRAME.registerComponent('markerhandler', {
 
+
+
     init: function() {
+
         const animatedMarker = document.querySelector("#links");
 
-        animatedMarker.addEventListener('click', function(ev){
-            if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
-                window.open("https://www.github.com");
-                /*if(ev.target.getAttribute('id') === "github"){
-                    
-                }
-                
-                const entity = document.querySelector('#animated-model');
-                const scale = entity.getAttribute('scale');
+        const aEntity = document.querySelector("#github");
+
+
+
+        // every click, we make our model grow in size :)
+
+        animatedMarker.addEventListener('click', function(ev, target){
+
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+
+            if (aEntity && intersectedElement === aEntity) {
+
+                const scale = aEntity.getAttribute('scale');
+
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                
-                // every click, we make our model grow in size
-                entity.setAttribute('scale', scale);
-                */
+
+                aEntity.setAttribute('scale', scale);
+
             }
+
         });
+
 }});

cb0730dfb793616a12adf12697073471ef1fc34e
Aidan Rubenstein <ar2126@rit.edu>
2019-02-25 11:05:45 -0500

    Spacing

--- events.js
+++ events.js
@@ -1,31 +1,17 @@
+//register the active marker
 AFRAME.registerComponent('markerhandler', {
 
-
-
     init: function() {
-
         const animatedMarker = document.querySelector("#links");
 
-
-
         animatedMarker.addEventListener('click', function(ev){
-
             if (animatedMarker.object3D.visible == true && ev.detail.cursorEl) {
-
                 const entity = document.querySelector('#animated-model');
-
                 const scale = entity.getAttribute('scale');
-
                 Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-
                 
-
                 // every click, we make our model grow in size
-
                 entity.setAttribute('scale', scale);
-
             }
-
         });
-
 }});
